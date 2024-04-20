0c78f244516a75e7ea4c43782d8c4e63375d2118
KasperiBlomberg <blomberg.kasperi@gmail.com>
2024-04-20 20:53:55 +0300

    Fix fly database connection

--- db.py
+++ db.py
@@ -2,10 +2,13 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
-#local
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+DATABASE_URL = getenv("DATABASE_URL")
 
-#fly.io
-#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
+FLY_DEPLOYMENT = getenv("FLY_DEPLOYMENT")
+
+if FLY_DEPLOYMENT:
+    DATABASE_URL = DATABASE_URL.replace("://", "ql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 
 db = SQLAlchemy(app)
