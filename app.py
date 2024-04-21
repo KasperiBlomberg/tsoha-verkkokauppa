6634be5ec59b1968d267c89bbcbea198739ad889
KasperiBlomberg <blomberg.kasperi@gmail.com>
2024-04-21 18:42:05 +0300

    Fix images on fly

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from os import getenv
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static", static_folder="static")
 app.secret_key = getenv("SECRET_KEY")
 
 import routes
